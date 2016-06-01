ae06b319f8bf9581157eaf7b5eed8e61df19e729
linqing-s <linqing-s@360.cn>
2016-06-01 19:08:37 +0800

    暂存

--- pie.js
+++ pie.js
@@ -1,23 +1,62 @@
-(function pie(){
-	$('.pie').each(function(){
-		var me = $(this);
-		var percent = me.attr('data-value');
-		me.text((percent * 100) + '%');
-		if (percent > 0.5) {
-			$('<span/>',{
-				'class' : 'span_1',
-				'style' : 'transform:rotate(0.5turn)'
-	        }).appendTo(me);
+var Pie = function(args){
 
-			$('<span/>',{
-				'class' : 'span_2',
-				'style' : 'display:block;transform:rotate('+ (percent-1) +'turn)'
-	        }).appendTo(me);
-		}else{
-			$('<span/>',{
-				'class' : 'span_1',
-				'style' : 'transform:rotate('+ percent +'turn)'
-	        }).appendTo(me);
-		}
-	});
-})();
+	var isAnimate = args.animite;
+
+	function _appendSpan(me,percent){
+		me.text((percent * 100).toFixed() + '%');
+		$('<span/>',{
+			'class' : 'span_1'
+        }).appendTo(me);
+
+		$('<span/>',{
+			'class' : 'span_2'
+        }).appendTo(me);
+	}
+
+	this.showPie = function(me,percent){
+		_appendSpan(me,percent);
+
+		setTimeout(function(){
+			if (percent > 0.5) {
+				$(me).children('.span_1').css({
+					'transform':'rotate(0.5turn)'
+				});
+				$(me).children('.span_2').css({
+					'opacity':1,
+					'transform':'rotate('+ (percent-0.5) +'turn)'
+				});
+			}else{
+				$(me).children('.span_1').css({
+					'transform':'rotate('+ percent +'turn)'
+				});
+			}
+		},0)
+
+
+	}
+
+	this.init = function(){
+		var self = this;
+		$(args.el).each(function(){
+			var me = $(this);
+			var percent = me.attr('data-value');
+			self.showPie(me,percent);
+		});
+	},
+	this.loading = function(){
+		var self = this;
+		// $(args.el).each(function(){
+		// 	var me = $(this);
+		// 	var percent = parseInt(me.attr('data-value'));
+		// 	setInterval(function(){
+		// 		me.attr('data-value',percent+=0.01);
+		// 		self.init();
+		// 	},100)
+		// });
+
+		
+	}
+};
+
+
+
